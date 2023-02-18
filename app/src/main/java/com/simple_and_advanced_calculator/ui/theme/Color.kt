f4e7c50f522797d51c5d90ef91f3ed0c541c3ddc
cyprian <cyprian@cyprians-MacBook-Pro.local>
2023-02-18 12:24:47 +0100

    add light mode and dark mode colours

--- app/src/main/java/com/simple_and_advanced_calculator/ui/theme/Color.kt
+++ app/src/main/java/com/simple_and_advanced_calculator/ui/theme/Color.kt
@@ -2,7 +2,14 @@ package com.simple_and_advanced_calculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+// Dark mode
+val Blue200 = Color(0xFFB6EADA) // text
+val Blue500 = Color(0xFF5B8FB9) // details
+val Dark500 = Color(0xFF301E67) // button
+val Dark700 = Color(0xFF03001C) // background
+
+// Light mode
+val White200 = Color(0xFFFFFBF5)  // background
+val White500 = Color(0xFFF7EFE5)  // button
+val Purple200 = Color(0xFFC3ACD0) // details
+val Purple500 = Color(0xFF674188) // text
